fc855b5810de0ee376789f86555a3db9a73fc789
AlessioMura <alessiomura03@gmail.com>
2024-10-10 15:03:20 +0200

    added countdown

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // gen random numbers
- 
+
 let rndm_number_1 = Math.floor(Math.random() * 100 + 1);
 let rndm_number_2 = Math.floor(Math.random() * 100 + 1);
 let rndm_number_3 = Math.floor(Math.random() * 100 + 1);
@@ -12,3 +12,17 @@ document.getElementById('random_number_2').innerHTML = rndm_number_2;
 document.getElementById('random_number_3').innerHTML = rndm_number_3;
 document.getElementById('random_number_4').innerHTML = rndm_number_4;
 document.getElementById('random_number_5').innerHTML = rndm_number_5;
+
+// countdown
+
+let timer = 30;
+
+let count_down = setInterval(function() {
+    if (timer <= 0) {
+        clearInterval(count_down);
+        document.getElementById('count_down').innerHTML = 'Write the numer you just saw.'
+    } else {
+        document.getElementById('count_down').innerHTML = timer;
+    }
+    timer -= 1;
+}, 1000);
