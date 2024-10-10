c47dbc74d200b0b27f205846e4d977496ef2ccdf
AlessioMura <alessiomura03@gmail.com>
2024-10-10 14:34:18 +0200

    random numbers fix

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-let rndm_number = Math.floor(Math.random() * 10 + 1);
+// gen random numbers
+ 
+let rndm_number_1 = Math.floor(Math.random() * 100 + 1);
+let rndm_number_2 = Math.floor(Math.random() * 100 + 1);
+let rndm_number_3 = Math.floor(Math.random() * 100 + 1);
+let rndm_number_4 = Math.floor(Math.random() * 100 + 1);
+let rndm_number_5 = Math.floor(Math.random() * 100 + 1);
 
-document.querySelector('random_number').innerHTML = rndm_number;
+
+document.getElementById('random_number_1').innerHTML = rndm_number_1;
+document.getElementById('random_number_2').innerHTML = rndm_number_2;
+document.getElementById('random_number_3').innerHTML = rndm_number_3;
+document.getElementById('random_number_4').innerHTML = rndm_number_4;
+document.getElementById('random_number_5').innerHTML = rndm_number_5;
